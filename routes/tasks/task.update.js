4a586221c46051e8d11dbdce1e07a624ab855787
Enestacy <evmagia@gmail.com>
2022-11-27 06:23:56 +0300

    Update user-tasks functionality

--- routes/tasks/task.update.js
+++ routes/tasks/task.update.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { Sequelize } = require("sequelize");
 const router = express.Router();
 const { defaultError } = require("../../errors");
 const db = require("../../models");
@@ -6,7 +7,7 @@ const db = require("../../models");
 router.patch("/mark-as-done", async function (req, res) {
   try {
     const {
-      body: { taskCode, userId },
+      body: { taskCode, userId, myRating },
     } = req;
 
     const updatedTask = await db.Task.findOne(
@@ -27,20 +28,28 @@ router.patch("/mark-as-done", async function (req, res) {
         plain: true,
       }
     );
+    const ratingCondition = myRating - currentUser.rating;
+    if (ratingCondition === 30 || ratingCondition === -30) {
+      await currentUser.addTask(updatedTask, { through: { isDone: true } })
+      const updatedUser = await db.User.update(
+        {
+          rating: Sequelize.literal(`rating + ${updatedTask.point}`),
+          where: {
+            id: userId,
+          },
+          include: db.Task,
+          returning: true,
+          plain: true,
+        }
+      );
 
-    await currentUser.addTask(updatedTask, { through: { isDone: true } })
-    const updatedUser = await db.User.findOne(
-      {
-        where: {
-          id: userId,
-        },
-        include: db.Task,
-        returning: true,
-        plain: true,
-      }
-    );
-
-    return res.send(updatedUser);
+      return res.send(updatedUser);
+    } else {
+      throw defaultError(
+        403,
+        "У вас нет на это прав! Разница в рейтинге должна составлять не менее 30 очков"
+      );
+    }
   } catch (error) {
     return res.status(error.status || 500).send(error);
   }
--- routes/tasks/tasks.get.js
+++ routes/tasks/tasks.get.js
@@ -4,23 +4,7 @@ const db = require("../../models");
 const { Tag, User, Task } = db;
 
 router.get("/tasks/:id", async function (req, res) {
-  const { body: { id } } = req
   try {
-    const user = await User.findOne({
-      where: {
-        id
-      },
-    })
-    if (user.status === 'quite') {
-      const tasks = await Task.findAll({
-        attributes: { exclude: ["tag"] },
-        where: {
-          category: "quite"
-        },
-        include: [{ model: Tag, attributes: { exclude: ["id"] } }],
-      });
-      return res.send(tasks);
-    }
     const tasks = await Task.findAll({
       attributes: { exclude: ["tag"] },
       include: [{ model: Tag, attributes: { exclude: ["id"] } }],
