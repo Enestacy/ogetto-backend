364925a90564f8eaa49219a2b52138b93f49ff28
Enestacy <evmagia@gmail.com>
2022-11-27 05:42:22 +0300

    Update getting tasks functionality

--- routes/tasks/tasks.get.js
+++ routes/tasks/tasks.get.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../models");
-const { Tag } = db;
+const { Tag, User, Task } = db;
 
-router.get("/tasks", async function (req, res) {
+router.get("/tasks/:id", async function (req, res) {
+  const { body: { id } } = req
   try {
-    const tasks = await db.Task.findAll({
+    const user = await User.findOne({
+      where: {
+        id
+      },
+    })
+    if (user.status === 'quite') {
+      const tasks = await Task.findAll({
+        attributes: { exclude: ["tag"] },
+        where: {
+          category: "quite"
+        },
+        include: [{ model: Tag, attributes: { exclude: ["id"] } }],
+      });
+      return res.send(tasks);
+    }
+    const tasks = await Task.findAll({
       attributes: { exclude: ["tag"] },
       include: [{ model: Tag, attributes: { exclude: ["id"] } }],
     });
